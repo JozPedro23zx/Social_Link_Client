c6c42e4579b6c1da950ca5fc04effaf8da46c0f3
José Pedro <josepedro23zx@gmail.com>
2022-05-05 18:20:10 -0300

    Update, but with issues

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,45 +1,54 @@
-// import { useState } from "react";
-// import ChatBox from "./sub-components/chatBox";
-// import '../customStyles/Chat.css'
+import io from "socket.io-client"
+import Axios from "axios"
+
+import { useState, useEffect } from "react";
+import '../customStyles/Chat.css'
 import image from '../images/coming-soon.jpg'
 
-function Chat() {
-    // const [username, setUsername] = useState("");
-    // const [room, setRoom] = useState("");
-    // const [showChat, setShowChat] = useState(false);    
-    // const joinRoom = () => {
-    //   if (username !== "" && room !== "") {
-    //     // socket.emit("join_room", room);
-    //     setShowChat(true);
-    //   }
-    // };  
-    return (
-      // <div>
-      //   {!showChat ? (
-      //     <div className="joinChatContainer">
-      //       <h3>Join A Chat</h3>
-      //       <input
-      //         type="text"
-      //         placeholder="John..."
-      //         onChange={(event) => {
-      //           setUsername(event.target.value);
-      //         }}
-      //       />
-      //       <input
-      //         type="text"
-      //         placeholder="Room ID..."
-      //         onChange={(event) => {
-      //           setRoom(event.target.value);
-      //         }}
-      //       />
-      //       <button onClick={joinRoom}>Join A Room</button>
-      //     </div>
-      //   ) : (
-      //     <ChatBox socket={"socket"} username={username} room={room} />
-      //   )}
-      // </div>
+import ChatBox from "./sub-components/chatBox";
+import UserChat from "./sub-components/usersChat"
 
-      <img src={image} alt="coming soon" style={{width: "100%", height: "auto"}}/>
+const socket = io.connect("http://localhost:8000")
+
+function Chat(props) {
+    const [showChat, setShowChat] = useState(false);    
+    const [roomsList, setRoomsList] = useState([]);
+    const [roomId, setRoomId] = useState('')
+
+  function rooms(){
+    Axios({
+      method: "GET",
+      withCredentials: true,
+      url: `${process.env.REACT_APP_API}/getAllRooms`,
+    }).then((res)=>{
+      setRoomsList(res.data)
+      console.log(res.data)
+    })
+  }
+  
+  const joinRoom = (room) => {
+      setRoomId(room)
+      socket.emit("join_room", room);
+      setShowChat(true);
+  };  
+
+  useEffect(() =>{
+    rooms()
+  }, [])
+
+    return (
+      <div>
+        <div className="chat-nav-bar">
+          {roomsList.map(room =>(
+            <UserChat room={room} userId={props.userId} joinRoom={joinRoom} />
+          ))}
+        </div>
+        <div>
+          {showChat ? <ChatBox socket={socket} roomId={roomId} userId={props.userId} /> : <></>}  
+        </div>
+      </div>
+
+      // <img src={image} alt="coming soon" style={{width: "100%", height: "auto"}}/>
     );
 }
 
